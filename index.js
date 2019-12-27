79715e5b3c39a336b2988515628ac8b567d79309
tangquan <tangquan@gamemania.cc>
2019-12-27 10:24:42 +0800

    add init status

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
-// 必须在文件头添加如上内容指定运行环境为node
-console.log('hello rambo cli');
+const commander = require('commander');
+const package = require('./package.json');
+const initAction = require('./commands/init')
+//版本号查询
+commander
+    .version(package.version)
+    .option('-v, --version','Check the version');
+
+//project init
+commander
+    .command('init [name]')
+    .option('-d, --dev','get develpment version')
+    .description('创建vue项目')
+    .action(initAction)
+
+// 利用commander解析命令行输入，必须写在所有内容最后面
+commander.parse(process.argv);
+
